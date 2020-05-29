8c363ac93e000da4e7c539b1f4cf2d4ae87896ec
adi-geek2 <adi2271188@gmail.com>
2020-05-29 14:33:19 +0200

    modified

--- hello_again.py
+++ hello_again.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-
-git status# imported libraries
-import numpy
-# main code
-print("Hello again")
-print("Hello again again")
-
-=======
 # imported libraries
 import numpy
 # main code
 print("Hello again")
 print("Hello again again")
 print("Byes")
->>>>>>> 428e29da84689a04e1802f0161e20f6884bb46ff
+
